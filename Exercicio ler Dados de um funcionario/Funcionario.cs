bda51f0c3841900cf8ace5e65dd180c5be71cfc7
Leandro Sousa <leandroscjTI@gmail.com>
2020-07-08 01:52:40 -0300

    Add project files.

---
+++ Exercicio ler Dados de um funcionario/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_ler_Dados_de_um_funcionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return Salario - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+             Salario = Salario + (Salario * porcentagem / 100.0);
+        }
+        public override string ToString()
+        {
+            return Nome + ", R$ " + SalarioLiquido().ToString("F2");
+
+
+
+        }
+    }
+}
---
+++ Exercicio ler Dados de um funcionario/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercicio_ler_Dados_de_um_funcionario
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario F = new Funcionario();
+
+            Console.WriteLine("Nome:");
+            F.Nome = Console.ReadLine();
+
+            Console.WriteLine("Salário bruto:");
+            F.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Imposto:");
+            F.Imposto = double.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("Funcionario: " +F);
+
+            Console.Write("Digite a porcentagem para aumentar:");
+            int p = int.Parse(Console.ReadLine());
+            F.AumentarSalario(p);
+
+            Console.WriteLine("Dados atualizados: " + F);
+
+
+
+
+
+        }
+    }
+}
